a37c6182b2a4b131339ba6d292c99c7beed04da4
Wystan Concepcion <concepcionwdc@aub.com.ph>
2021-08-18 13:24:51 +0800

    Initial commit

---
+++ Bayad Promos/Bayad Promos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bayad Promos
+//
+//  Created by Application Developer 9 on 8/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
